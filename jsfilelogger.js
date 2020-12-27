c1129f9f57b0c3d602b1fd65eeb1c7048649a178
Delphren Phox <noahthemlg.yt@gmail.com>
2020-12-27 15:33:56 -0600

    Broken
    Broken

--- jsfilelogger.js
+++ jsfilelogger.js
@@ -1,4 +1,9 @@
 fs = require("fs")
+const colorsfile = ("colors.json")
+var colors
+var colorsunparsed
+colorsunparsed = (fs.readFileSync(colorsfile, 'utf8'))
+colors = JSON.parse(colorsunparsed)
 const startTime = new Date()
 if(!fs.existsSync("logs")){
     fs.mkdirSync("logs", 0766, function(err){
@@ -9,8 +14,12 @@ if(!fs.existsSync("logs")){
         }
     });
 }
-exports.log = function(directinput){
+
+exports.log = function(input,colors){
 	try{
+		if(colors.length == 0){
+			colors = [Reset]
+		}
 		var input = directinput
 		var fileDate = (startTime.getFullYear() + "-" + (startTime.getMonth() + 1) + "-" + startTime.getDate() + "-" + startTime.getHours() + ";" + startTime.getMinutes() + ";" + startTime.getSeconds() + ";" + startTime.getMilliseconds())
 		var fileName = ("logs/" + fileDate + ".log")
@@ -21,9 +30,13 @@ exports.log = function(directinput){
 		fs.appendFile(fileName, fileoutputstring, function (error) {
 			if (error) throw error;
 		});
+		var colorcount = colors.length - 1
+		for(colors.length = 0; colors.length < 1; colors.length++){
+			colors.length = colors.length + consoleoutputstring
+		}
 		console.log(consoleoutputstring)
 		}
 	catch(error){
-		lowlevelerrhandle(error)
+		console.log(error)
 	}
 }
