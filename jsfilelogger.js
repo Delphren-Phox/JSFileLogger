9ba7ec1bae7897b09ddd2274a9803f9430d45b61
Delphren Phox <noahthemlg.yt@gmail.com>
2020-12-28 17:16:52 -0600

    Removed Color code

--- jsfilelogger.js
+++ jsfilelogger.js
@@ -1,9 +1,4 @@
 fs = require("fs")
-const colorsfile = ("colors.json")
-var colors
-var colorsunparsed
-colorsunparsed = (fs.readFileSync(colorsfile, 'utf8'))
-colors = JSON.parse(colorsunparsed)
 const startTime = new Date()
 if(!fs.existsSync("logs")){
     fs.mkdirSync("logs", 0766, function(err){
@@ -15,12 +10,8 @@ if(!fs.existsSync("logs")){
     });
 }
 
-exports.log = function(input,colors){
+exports.log = function(input){
 	try{
-		if(colors.length == 0){
-			colors = [Reset]
-		}
-		var input = directinput
 		var fileDate = (startTime.getFullYear() + "-" + (startTime.getMonth() + 1) + "-" + startTime.getDate() + "-" + startTime.getHours() + ";" + startTime.getMinutes() + ";" + startTime.getSeconds() + ";" + startTime.getMilliseconds())
 		var fileName = ("logs/" + fileDate + ".log")
 		var commandDate = new Date()
@@ -30,13 +21,10 @@ exports.log = function(input,colors){
 		fs.appendFile(fileName, fileoutputstring, function (error) {
 			if (error) throw error;
 		});
-		var colorcount = colors.length - 1
-		for(colors.length = 0; colors.length < 1; colors.length++){
-			colors.length = colors.length + consoleoutputstring
-		}
 		console.log(consoleoutputstring)
 		}
 	catch(error){
+		console.log("JSFILELOGGER ERROR")
 		console.log(error)
 	}
 }
